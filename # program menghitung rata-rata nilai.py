470659a5d99428bc58f1b8909921780b3e78a50b
maulanaHR-spec <maulana.rtg15@gmail.com>
2026-01-11 08:24:43 +0700

    Add files via upload

---
+++ # program menghitung rata-rata nilai.py
@@ -0,0 +1,11 @@
+# program menghitung rata-rata nilai
+# Meminta input nilai ujian dari pengguna
+matematika=float(input("Masukkan nilai Matematika :"))
+fisika=float(input("Masukkan nilai Fisika :"))
+kimia=float(input("Masukkan nilai Kimia :"))
+
+# Menghitung rata-rata
+rata_rata=(matematika+fisika+kimia) / 3
+
+# Menampilkan hasil rata-rata
+print("Rata-rata nilai ujian adalah :",rata_rata)
