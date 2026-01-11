977af1732bce919de93835c3da3d08b400285021
maulanaHR-spec <maulana.rtg15@gmail.com>
2026-01-11 08:31:01 +0700

    Add files via upload

---
+++ #program kalkulator sederhana.py
@@ -0,0 +1,17 @@
+#program kalkulator sederhana
+#meminta input dua bilangan dari pengguna
+bilangan1=float(input("masukan bilangan pertama :"))
+bilangan2=float(input("masukan bilangan kedua :"))
+#melakukan operasi aritmatika
+penjumlahan=bilangan1+bilangan2
+pengurangan=bilangan1-bilangan2
+perkalian=bilangan1*bilangan2
+pembagian=bilangan1/bilangan2
+modulus=bilangan1%bilangan2
+#menampilkan hasil setiap operasi
+print("Hasil Operasi Aritmatika ")
+print("penjumlahan :",penjumlahan)
+print("pengurangan :",pengurangan)
+print("perkalian :",perkalian)
+print("pembagian :",pembagian)
+print("modulus :",modulus)
